d5b64576639337cc217c760893e99a88d88c09d9
Anurag Goel <anurag@render.com>
2019-04-07 13:58:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Gin Web Server
+
+This repo can be used as a starting point to deploy [Go](https://golang.org/) web applications on Render.
+
+It is based on the [realtime chat](https://github.com/gin-gonic/examples/tree/master/realtime-advanced) example powered by the [Gin](https://github.com/gin-gonic/gin) web framework.
+
+The sample app is up at https://go-gin.onrender.com.
+
+## Deployment
+
+See the guide at https://render.com/docs/deploy-go-gin-web-server.
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	ConfigRuntime()
+	StartWorkers()
+	StartGin()
+}
+
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
+
+// StartWorkers start starsWorker by goroutine.
+func StartWorkers() {
+	go statsWorker()
+}
+
+// StartGin starts gin web server with setting router.
+func StartGin() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	router.Use(rateLimit, gin.Recovery())
+	router.LoadHTMLGlob("resources/*.templ.html")
+	router.Static("/static", "resources/static")
+	router.GET("/", index)
+	router.GET("/room/:roomid", roomGET)
+	router.POST("/room-post/:roomid", roomPOST)
+	router.GET("/stream/:roomid", streamRoom)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+        log.Panicf("error: %s", err)
+	}
+}
